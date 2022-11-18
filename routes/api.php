434444126fe8c14c93739573c08c57639d848e41
Grymore <37777233+Grymore@users.noreply.github.com>
2022-11-19 00:59:55 +0700

    validasi notify

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::post('/customer', [CustomerController::class, "store"] );
 Route::put('/customer/{id}', [CustomerController::class, "update"]);
 Route::get('/customer/{id}', [CustomerController::class, "show"]);
 Route::delete('/customer/{id}', [CustomerController::class, "destroy"]);
+Route::post('/notify', [CustomerController::class, "updateStatus"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
